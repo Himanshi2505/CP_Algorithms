ea1d481ef50b14597cd1bc4465cf2108d3617986
Himanshi Tibrewal <himanshitibrewal25@gmail.com>
2024-12-26 13:06:00 +0530

    some more practice

---
+++ CP31 Sheet/Two Permutations.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,a,b;
+        cin>>n>>a>>b;
+       if(a+b>=n-1){
+        if(a==n && b==n){
+            cout<<"Yes"<<endl;
+        }
+        else cout<<"No"<<endl;
+       }
+       else cout<<"yes"<<endl;
+    }
+    return 0;
+}
