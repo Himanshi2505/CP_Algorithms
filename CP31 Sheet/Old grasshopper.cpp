a5fdbe6dc3ea7f8917f9bfa6a7ad7bd8e0159201
Himanshi Tibrewal <himanshitibrewal25@gmail.com>
2025-06-06 11:49:50 +0530

    old grasshopper

---
+++ CP31 Sheet/Old grasshopper.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long x0,n;
+        cin>>x0>>n;
+
+        long long d;
+        int r = n % 4;
+        if(r==0) d = 0;
+        else if (r==1) d = n;
+        else if( r == 2) d = -1;
+        else d = -n -1;
+
+        if(x0 % 2 == 0){
+            cout<< x0 - d <<endl;
+        }
+        else{
+            cout<< x0 + d <<endl;
+        }
+    }
+    return 0;
+}
