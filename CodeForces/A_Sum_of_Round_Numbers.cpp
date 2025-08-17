c23b38d8fe1aa07ac2731c78aa7f6bb588b1150c
Himanshi2505 <himanshitibrewal25@gmail.com>
2025-08-17 16:17:10 +0530

    practice

--- CodeForces/A_Sum_of_Round_Numbers.cpp
+++ CodeForces/A_Sum_of_Round_Numbers.cpp
@@ -6,13 +6,31 @@ using namespace std;
 const int mod = 1e9 + 7;
  
 void solve() {
-  
+  int n;
+  cin>>n;
+  vector<int> v;
+  int y=1;
+  while(n>0){
+    int x = n%10;
+    if(x!=0){
+        x = x*y;
+        v.push_back(x);
+    }
+    y *= 10;
+    n /= 10;
+  }
+  cout<<v.size()<<endl;
+  for(int i=0;i<v.size();i++){
+    cout<<v[i]<<" ";
+  }
+  cout<<endl;
 }   
  
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int t = 1;
+    cin>>t;
     while (t--) {
         solve();
     }
